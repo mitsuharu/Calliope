62a8721116343477f4e93a1e8a755ae2f465ad5a
Mitsuharu Emoto <mthr1982@gmail.com>
2024-05-04 23:22:50 +0900

    fix main view temp

--- Calliope/View/MainView/MainView.swift
+++ Calliope/View/MainView/MainView.swift
@@ -11,13 +11,28 @@ struct MainView: View {
     @ObservedObject private var viewModel = MainViewModel()
 //    let printerUseCase = PrinterUseCase()
     
+    private let columns: [GridItem] = Array(repeating: .init(.flexible()),
+                                            count: 2)
+    
     var body: some View {
         VStack {
-            Text("MainView")
-            Spacer().frame(height: 10)
-            Text("name: \(viewModel.name ?? "-")")
-            Spacer().frame(height: 10)
-            Text("uuid: \(viewModel.uuid ?? "-")")
+            VStack{
+                Text("name: \(viewModel.name ?? "-")")
+                Spacer().frame(height: 10)
+                Text("uuid: \(viewModel.uuid ?? "-")")
+            }.padding()
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach((0..<8), id: \.self) { _ in
+                        Button(action: {
+                            // do anything
+                        }) {
+                            DoPrintCell()
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+            }
         }
     }
 }
